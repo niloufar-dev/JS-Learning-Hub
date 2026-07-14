ccfb10c9c2d284fafcd4542c8b256cdf16506d5e
Niloufar <mohammadiniloofar80@gmail.com>
2026-07-14 16:59:41 +0330

    complete FAQ accordion

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const darkmod=document.querySelector('.dark-mod')
+const question=document.querySelector('.qu')
+
+
 darkmod.addEventListener('click',()=>{
       document.documentElement.classList.toggle("dark");
       if(document.documentElement.classList.contains('dark')){
@@ -6,5 +9,33 @@ darkmod.addEventListener('click',()=>{
       }else{
         darkmod.innerHTML='🌙'
       }
+    
+})
+
+question.addEventListener('click',(e)=>{
+    const ask = e.target.closest('.ask')
+    
+    const btn=ask.querySelector('button')
+    console.log(btn)
+    
+    const answer= ask.nextElementSibling
+    const open=answer.style.maxHeight
+    const ans = document.querySelectorAll('.ans')
+    const assigns=document.querySelectorAll('.ask button')
+    console.log(assigns);
+    
+    console.log(ans);
+    
+
+    ans.forEach((item)=>{
+    item.style.maxHeight=''
+    assigns.forEach((val)=>{
+        val.style.rotate='0deg'
+    })
+    })
+    if(!open){
+        answer.style.maxHeight = answer.scrollHeight + "px";
+        btn.style.rotate='45deg'
+    }
     
 })
